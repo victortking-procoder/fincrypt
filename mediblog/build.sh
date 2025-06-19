190d5d5a0c0b8813c39901b43a424d62adbef1d2
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-19 21:57:40 +0100

    render redeploy

--- mediblog/build.sh
+++ mediblog/build.sh
@@ -3,8 +3,9 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Apply migrations
-python manage.py migrate --noinput
-
 # Collect static files
 python manage.py collectstatic --noinput
+
+
+# Apply migrations
+python manage.py migrate --noinput
